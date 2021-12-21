801ec0d65affcf5ea8640006b1f1a56a34a03e7c
Dennis Hoppe <github@dhoppe.it>
2021-12-22 00:48:21 +0100

    feat: Disable VCS status updates for untriggered speculative plans

--- main.tf
+++ main.tf
@@ -1,11 +1,12 @@
 resource "tfe_organization" "default" {
-  name                     = var.name
-  email                    = var.email
-  session_timeout_minutes  = var.session_timeout_minutes
-  session_remember_minutes = var.session_remember_minutes
-  collaborator_auth_policy = var.collaborator_auth_policy
-  owners_team_saml_role_id = var.owners_team_saml_role_id
-  cost_estimation_enabled  = var.cost_estimation_enabled
+  name                                                    = var.name
+  email                                                   = var.email
+  session_timeout_minutes                                 = var.session_timeout_minutes
+  session_remember_minutes                                = var.session_remember_minutes
+  collaborator_auth_policy                                = var.collaborator_auth_policy
+  owners_team_saml_role_id                                = var.owners_team_saml_role_id
+  cost_estimation_enabled                                 = var.cost_estimation_enabled
+  send_passing_statuses_for_untriggered_speculative_plans = var.send_passing_statuses_for_untriggered_speculative_plans
 }
 
 resource "tfe_organization_membership" "default" {
--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     tfe = {
       source  = "hashicorp/tfe"
-      version = ">= 0.26.1, < 1.0.0"
+      version = ">= 0.27.0, < 1.0.0"
     }
   }
 
